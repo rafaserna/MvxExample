568158f52cb70bb16d765202dad7129704e7aed3
Rafa Serna <rafaserna@outlook.com>
2016-01-10 19:24:44 +0100

    Añadido proyecto universal para Win10

---
+++ MvxExample.UWP/Setup.cs
@@ -0,0 +1,19 @@
+﻿namespace MvxExample.UWP
+{
+    using Cirrious.MvvmCross.ViewModels;
+    using Cirrious.MvvmCross.WindowsCommon.Platform;
+    using Cirrious.MvvmCross.WindowsCommon.Views;
+    using Windows.UI.Xaml.Controls;
+
+    public class Setup : MvxWindowsSetup
+    {
+        public Setup(Frame rootFrame) : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+    }
+}
---
+++ MvxExample.UWP/Views/StartView.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MvxExample.UWP.Views
+{
+    using Cirrious.MvvmCross.WindowsCommon.Views;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.InteropServices.WindowsRuntime;
+    using Windows.Foundation;
+    using Windows.Foundation.Collections;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Controls.Primitives;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Navigation;
+
+    public sealed partial class StartView : MvxWindowsPage
+    {
+        public StartView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
